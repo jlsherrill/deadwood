c8ffc054f2b2752d66015f6292523fc51b1e4879
Chris Alfonso <calfonso@redhat.com>
2012-08-22 10:24:50 -0400

    Adding activation_key update support

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem "rest-client"
 gem "nokogiri"
 gem "vcr"
 gem "rspec"
-
 group :development do
 end
 
--- spec/models/activation_key_spec.rb
+++ spec/models/activation_key_spec.rb
@@ -40,4 +40,36 @@ describe Deadwood::Katello::ActivationKey do
       env.destroy
     end
   end
+
+  it "should create an activation key and be able to search for it by id and then destroy it" do
+    VCR.use_cassette 'create_find_by_id_activation_key' do
+      env = Deadwood::Katello::Environment.new(:name => 'Test', :prior => 1, :organization_id => org_id)
+      env.save
+      activation_key = Deadwood::Katello::ActivationKey.new(:name => 'test_activation_key', :description => 'test_description', :environment_id => env.id)
+      activation_key.save
+      activation_key.nil?.should be_false
+      activation_key = Deadwood::Katello::ActivationKey.find(activation_key.id)
+      activation_key.nil?.should be_false
+      activation_key.destroy
+      env.destroy
+    end
+  end
+
+  it "should create an activation key and move it from one environment to another, then destroy it" do
+    VCR.use_cassette 'create_update_activation_key' do
+      env_a = Deadwood::Katello::Environment.new(:name => 'Test A', :prior => 1, :organization_id => org_id)
+      env_a.save
+      env_b = Deadwood::Katello::Environment.new(:name => 'Test B', :prior => env_a.id, :organization_id => org_id)
+      env_b.save
+      activation_key = Deadwood::Katello::ActivationKey.new(:name => 'test_activation_key', :description => 'test_description', :environment_id => env_a.id)
+      activation_key.save
+      activation_key.nil?.should be_false
+      activation_key.update_attribute(:environment_id, env_b.id)
+      activation_key = Deadwood::Katello::ActivationKey.find(activation_key.id)
+      (activation_key.environment_id == env_b.id).should be_true
+      activation_key.destroy
+      env_b.destroy
+      env_a.destroy
+    end
+  end
 end
